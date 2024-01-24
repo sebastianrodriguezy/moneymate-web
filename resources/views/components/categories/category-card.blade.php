a667874f4dac24eda0f14af744767861e5efdb15
Juan Sebastian Rodriguez Yaima <sky.sebas.2020@gmail.com>
2024-01-24 17:35:12 -0500

    [fix] updated styles and wrong icons

--- resources/views/components/categories/category-card.blade.php
+++ resources/views/components/categories/category-card.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full max-h-[250px] overflow-y-auto overflow-x-hidden">
+<div class="w-full max-h-[280px] overflow-y-auto overflow-x-hidden">
   <div class="w-full py-4 text-gray-900 dark:text-white">
 
     <template x-if="!dataCalc && !isLoadingCalc && errorInCalc">
@@ -12,8 +12,9 @@
     </template>
 
     <template x-for="category in dataCalc?.rows || []">
-      <div class="w-full flex flex-row  items-center gap-3 rounded-md bg-slate-100 dark:bg-gray-700 mb-3 p-3">
-        <ion-icon name="pricetag" class="mr-2 text-gray-300/50 dark:text-gray-200/50"></ion-icon>
+      <div
+        class="w-full flex flex-row  items-center gap-3 rounded-t-md hover:bg-slate-100 border-b dark:border-b-gray-700 dark:hover:bg-gray-700 p-3">
+        <ion-icon name="pricetag" class="mr-2 text-teal-500/80"></ion-icon>
         <span x-text="category.name"></span>
 
         <span class="ml-auto" x-text="formatDate(category.date)"></span>
--- resources/views/components/movements/movement-card.blade.php
+++ resources/views/components/movements/movement-card.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full max-h-[250px] overflow-y-auto overflow-x-hidden">
+<div class="w-full max-h-[280px] overflow-y-auto overflow-x-hidden">
   <div class="w-full py-4 text-gray-900 dark:text-white">
 
     <template x-if="!dataCalc && !isLoadingCalc && errorInCalc">
@@ -12,7 +12,8 @@
     </template>
 
     <template x-for="movement in dataCalc?.rows || []">
-      <div class="w-full flex flex-row  items-center gap-3 rounded-md bg-slate-100 dark:bg-gray-700 mb-3 p-3">
+      <div
+        class="w-full flex flex-row items-center gap-3 rounded-t-md hover:bg-slate-100 border-b dark:border-b-gray-700 dark:hover:bg-gray-700 p-3">
         <ion-icon x-bind:name="movement.type === 'income' ? 'arrow-up' : 'arrow-down'"
           x-bind:class="movement.type === 'income' ? 'text-green-500' : 'text-red-500'"></ion-icon>
         <span class="font-lato tracking-wider" x-text="formatStringToMoney(movement.amount, movement.type)"></span>
